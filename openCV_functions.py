804f4d539681e7bf8c3da5d1b4d5c950a13d6f53
Ndichu Mwangi <ndichumwangi7@gmail.com>
2022-11-16 09:07:48 +0300

    init comit

---
+++ openCV_functions.py
@@ -0,0 +1,30 @@
+import cv2 as cv
+
+img = cv.imread('pics/1.jpg')
+cv.imshow('Cat', img)
+
+# # converting to grayscale
+# gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# cv.imshow('Gray', gray)
+
+# #Blur
+# blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
+# cv.imshow('Blur', blur)
+
+# #Edge cascade
+# canny = cv.Canny(img, 125, 175)
+# cv.imshow('Canny Edges', canny)
+#
+# #Dilating the image
+# dilated = cv.dilate(canny, (7,7), iterations=3)
+# cv.imshow('Dilated', dilated)
+#
+# #Eroding
+# eroded = cv.erode(dilated, (7,7), iterations=3)
+# cv.imshow('Eroded', eroded)
+
+resized = cv.resize(img, (500, 500), interpolation=cv.INTER_CUBIC)
+cv.imshow('Resized', resized)
+
+
+cv.waitKey(0)
